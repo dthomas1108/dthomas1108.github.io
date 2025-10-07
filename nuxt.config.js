e9267ab7ece8fd071e48d1b461afd304efe8b00a
Dylan Thomas <dylanthomas1108@gmail.com>
2025-10-07 13:39:27 +0100

    initial setup/commit

---
+++ nuxt.config.js
@@ -0,0 +1,25 @@
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './app/components/ui'
+  },
+})
