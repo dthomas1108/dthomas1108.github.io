8e2b1f867998e809a57a82686c0d28022a2756b5
Dylan Thomas <dylanthomas1108@gmail.com>
2025-11-11 21:32:00 +0000

    set up files

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
+  <NuxtLayout>
     <NuxtPage />
-  </div>
+  </NuxtLayout>
 </template>
 
 <script setup>
+useHead({
+  title: 'Dylan Thomas - Software Engineer Portfolio',
+  meta: [
+    { name: 'description', content: 'I write code, solve problems and sometimes make things that actually work. Welcome to my portfolio!' }
+  ]
+})
 </script>
